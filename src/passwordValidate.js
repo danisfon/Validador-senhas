e300ec0d13e289b4e76a484d3b1d29440eb5037a
danisfon <danieledasilvafonseca@gmail.com>
2026-04-16 18:39:08 -0300

    feat: validação  mínimo uma letra maiúscula

--- src/passwordValidate.js
+++ src/passwordValidate.js
@@ -1,6 +1,7 @@
 const passwordValidate = (password) => {
     if (password.length < 8) return false;
     if (!/[A-Z]/.test(password)) return false;
+    if (!/[a-z]/.test(password)) return false;
     return true;
 }
 
