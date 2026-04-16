93e3d82fd366be9ae90442ac66c35ee4310b26e0
danisfon <danieledasilvafonseca@gmail.com>
2026-04-16 18:44:48 -0300

    feat: validação sem espaços

--- src/passwordValidate.js
+++ src/passwordValidate.js
@@ -3,7 +3,8 @@ const passwordValidate = (password) => {
     if (!/[A-Z]/.test(password)) return false;
     if (!/[a-z]/.test(password)) return false;
     if (!/[0-9]/.test(password)) return false;
-    if (!/[!@#$%^&*]/.test(password)) return false;
+    if (!/[!@#$%^&*]/.test(password)) return false; 
+    if (/\s/.test(password)) return false;
     return true;
 }
 
