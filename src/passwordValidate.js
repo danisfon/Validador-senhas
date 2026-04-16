ebfbc59354defeccf65369ca921d23f9042ed56a
danisfon <danieledasilvafonseca@gmail.com>
2026-04-16 18:40:47 -0300

    feat:validação  mínimo uma número

--- src/passwordValidate.js
+++ src/passwordValidate.js
@@ -2,6 +2,7 @@ const passwordValidate = (password) => {
     if (password.length < 8) return false;
     if (!/[A-Z]/.test(password)) return false;
     if (!/[a-z]/.test(password)) return false;
+    if (!/[0-9]/.test(password)) return false;
     return true;
 }
 
