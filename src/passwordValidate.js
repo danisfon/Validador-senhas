8cb9b7772e80484b026812abd90a07b858254aaa
danisfon <danieledasilvafonseca@gmail.com>
2026-04-16 20:10:23 -0300

    refactor: melhorar legibilidade da validação de senha

--- src/passwordValidate.js
+++ src/passwordValidate.js
@@ -1,11 +1,19 @@
+const hasMinLength = (password) => password.length >= 8;
+const hasUpperCase = (password) => /[A-Z]/.test(password);
+const hasLowerCase = (password) => /[a-z]/.test(password);
+const hasNumber = (password) => /[0-9]/.test(password);
+const hasSpecialChar = (password) => /[!@#$%^&*]/.test(password);
+const hasNoSpaces = (password) => !/\s/.test(password);
+
 const passwordValidate = (password) => {
-    if (password.length < 8) return false;
-    if (!/[A-Z]/.test(password)) return false;
-    if (!/[a-z]/.test(password)) return false;
-    if (!/[0-9]/.test(password)) return false;
-    if (!/[!@#$%^&*]/.test(password)) return false; 
-    if (/\s/.test(password)) return false;
-    return true;
-}
+  return (
+    hasMinLength(password) &&
+    hasUpperCase(password) &&
+    hasLowerCase(password) &&
+    hasNumber(password) &&
+    hasSpecialChar(password) &&
+    hasNoSpaces(password)
+  );
+};
 
 module.exports = passwordValidate;
